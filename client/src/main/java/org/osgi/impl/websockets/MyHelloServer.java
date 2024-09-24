93ae787ecaae7d97ef12054aa2db74f0eef5bbcd
Christoph Läubrich <christoph@laeubi-soft.de>
2024-09-24 17:35:35 +0200

    Add example client from the spec

---
+++ client/src/main/java/org/osgi/impl/websockets/MyHelloServer.java
@@ -0,0 +1,17 @@
+package org.osgi.impl.websockets;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.jakarta.websocket.whiteboard.propertytypes.WhiteboardEndpoint;
+
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.server.ServerEndpoint;
+
+@WhiteboardEndpoint
+@ServerEndpoint("/hello")
+@Component(service = MyHelloServer.class, immediate = true)
+public class MyHelloServer {
+	@OnMessage
+	public String handleMessage(String message) {
+		return "Got your message (" + message + "). Thanks !";
+	}
+}
